c0fa296fb106532af3e9f5a9fcac2ee68657d88c
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-12-26 00:54:45 -0300

    feat: create compose file, new app and postgres connection
    39:00

--- videos/settings.py
+++ videos/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = "videos.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mydb",
+        "USER": "postgres",
+        "PASSWORD": "root",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
